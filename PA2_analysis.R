6927ae0d6e0f8b9a599c7ccadf1d3d81bc2b4792
stubz <yamamoto.yusuke+github@gmail.com>
2014-08-23 01:34:18 +0900

    first commit

---
+++ PA2_analysis.R
@@ -0,0 +1,58 @@
+setwd("/Users/okada/myWork/coursera/RepData_PeerAssessment2")
+library(plyr);library(ggplot2)
+#dat <- read.csv("repdata-data-StormData.csv")
+dat <- read.csv(bzfile("repdata-data-StormData.csv.bz2", open="r"))
+dim(dat);head(dat)
+save(dat, file="dat.RData")
+load("dat.RData")
+
+
+# Across the United States, which types of events (as indicated in the EVTYPE variable) 
+# are most harmful with respect to population health?
+
+# Across the United States, which types of events have the greatest economic consequences?
+
+
+## Data processing
+# def can be found in http://forecast.weather.gov/glossary.php?word=wfo
+# WFO: Weather Forecast Office
+# PROPDMG : property damage
+# CROPDMG : crop damage
+
+## summarise fatalities and injuries by event type
+dat.sum.evtype <- ddply(dat, .(EVTYPE), summarize, 
+                        fatalities=sum(FATALITIES), injuries=sum(INJURIES), 
+                        total_health_harm = sum(FATALITIES+INJURIES),
+                        propdmg = sum(PROPDMG), cropdmg=sum(CROPDMG),
+                        total_economic_dmg = sum(PROPDMG+CROPDMG)
+                        )
+dat.sum.evtype <- arrange(dat.sum.evtype, desc(total_health_harm), desc(fatalities), desc(injuries))
+head(dat.sum.evtype)
+## worst 5 event : Tornado, excessive heat, Thunderstorm(TSTM) wind, flood, lightning
+
+
+## economic damge can be estimated by PROPDMG and CROPDMG
+dat.sum.evtype <- arrange(dat.sum.evtype, desc(total_economic_dmg),
+                          desc(propdmg), desc(cropdmg))
+head(dat.sum.evtype)
+## tornado, flash flood, tstm wind, hail, flood
+arrange(dat.sum.evtype, desc(cropdmg), desc(total_economic_dmg),
+        desc(propdmg))[1:5, ]
+arrange(dat.sum.evtype, desc(propdmg), desc(total_economic_dmg),
+        desc(cropdmg))[1:5, ]
+library(reshape2)
+dat.melt <- melt(subset(dat.sum.evtype,  
+                        select=-c(total_health_harm,total_economic_dmg)),
+                 id.vars=c("EVTYPE"), variable.name="dmg_type", na.rm=TRUE)
+## health damage
+m1 <- ggplot(subset(dat.melt, EVTYPE %in% c("EXCESSIVE HEAT","HEAT", "LIGHTNING","HAIL","TORNADO","FLASH FLOOD","TSTM WIND","FLOOD","THUNDERSTORM WIND") & 
+                      dmg_type %in% c("injuries","fatalities")),
+             aes(x=EVTYPE, y=value, fill=factor(dmg_type)))
+m1 + geom_bar(stat="identity" )+coord_flip()
+
+
+## economic damage
+m2 <- ggplot(subset(dat.melt, EVTYPE %in% c("EXCESSIVE HEAT","HEAT", "LIGHTNING","HAIL","TORNADO","FLASH FLOOD","TSTM WIND","FLOOD","THUNDERSTORM WIND") & 
+                      dmg_type %in% c("propdmg","cropdmg")),
+             aes(x=EVTYPE, y=value, fill=factor(dmg_type)))
+m2 + geom_bar(stat="identity" )+coord_flip()+ scale_fill_grey()
